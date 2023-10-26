58e1d11693c89cf2b367a442d659b8178fe23ac1
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-10-27 00:56:06 +0400

    Added heap for optimization

--- Pathfinding/Assets/Scripts/Node.cs
+++ Pathfinding/Assets/Scripts/Node.cs
@@ -9,6 +9,7 @@ public class Node
     public int gCost, hCost;
 
     public Node parent;
+    private int heapIndex;
     public int fCost => gCost + hCost;
 
     public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
@@ -19,4 +20,20 @@ public class Node
         this.gridY = gridY;
     }
 
+    public int HeapIndex
+    {
+        get => heapIndex;
+        set => heapIndex = value;
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+
+        return -compare;
+    }
 }
